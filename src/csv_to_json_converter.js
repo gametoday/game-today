c1d142ed7c2c394b45a2601f46a95abe2a181c1d
Caroline Wong <carowong5@gmail.com>
2019-07-13 15:41:39 -0700

    Update csv_to_json_converter download data

--- src/csv_to_json_converter.js
+++ src/csv_to_json_converter.js
@@ -12,13 +12,16 @@ const keyMap = {
   endTimeET: 'END TIME ET',
 };
 
-function csvToJson() {
-  const csvFilePath='../data/giants.csv';
+function csvToJson(teamName) {
+  const csvFilePath=`./data/${teamName}.csv`;
+  console.log('csv path', csvFilePath);
 
   csv()
     .fromFile(csvFilePath)
     .then(array => {
-      return arrayToObjectDateKey(array);
+      const obj = arrayToObjectDateKey(array);
+      saveObj(obj, teamName);
+      // saveToOwnFile(obj, teamName);
     })
 }
 
@@ -31,10 +34,9 @@ function csvToJson() {
         location: 'Oracle Park - San Francisco',
         subject: 'Dodgers at Giants'
       }
-**/
+ **/
 function arrayToObjectDateKey(gameArray) {
   const obj = {};
-
   gameArray.forEach(game => {
     const gameData = {
       startDate: game[keyMap.startDate],
@@ -45,19 +47,73 @@ function arrayToObjectDateKey(gameArray) {
     };
     obj[gameData.startDate] = gameData;
   });
-  fs.writeFile("giants.json", JSON.stringify(obj), 'utf8', function (err) {
-    console.log('in here');
-        if (err) {
-                  console.log("An error occured while writing JSON Object to File.");
-                  return console.log(err);
-              }
-
-        console.log("JSON file has been saved.");
-  });
-
   return obj;
 }
 
- csvToJson();
+function saveToOwnFile(obj, teamName) {
+  var json = JSON.stringify(obj);
+  const jsonFileName=`./data/${teamName}.json`;
+  fs.writeFile(jsonFileName, json, () => {});
+};
+
+function saveObj(obj, teamName) {
+  fs.exists('gameData.json', function(exists) {
+    if (exists) {
+      console.log("yes file exists for", teamName);
+      var data = fs.readFileSync('gameData.json');
+      existingData = JSON.parse(data);
+      console.log('can parse data');
+      existingData[teamName] = obj;
+      console.log('exist');
+      var json = JSON.stringify(existingData);
+      fs.writeFile('gameData.json', json, () => {});
+      /**
+      fs.readFileSync('gameData.json', readFileCallback(err, data) => {
+        if (err) {
+          console.log('ERR', err);
+        } else {
+          existingData = JSON.parse(data);
+          console.log('can parse data');
+          existingData[teamName] = obj;
+          console.log('exist');
+          var json = JSON.stringify(existingData);
+          fs.writeFile('gameData.json', json, () => {});
+        }
+      });
+    **/
+    } else {
+      console.log("file not exists")
+      const newData = {
+        [teamName]: obj
+      };
+      var json = JSON.stringify(newData);
+      fs.writeFileSync('gameData.json', json);
+    }
+  });
+};
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const MLB_TEAMS_LOWERCASE = ["diamondbacks", "braves", "orioles", "red sox", "cubs", "white sox", "reds", "indians", "rockies", "tigers", "astros", "royals", "angels", "dodgers", "marlins", "brewers", "twins", "mets", "yankees", "athletics", "phillies", "pirates", "padres", "giants", "mariners", "cardinals", "rays", "rangers", "blue jays", "nationals"];
+// const MLB_TEAMS_LOWERCASE = ["diamondbacks", "braves"];
+
+for (let i = 0; i < MLB_TEAMS_LOWERCASE.length; i++) {
+    setTimeout(() => {
+      let team = MLB_TEAMS_LOWERCASE[i];
+      csvToJson(team);
+      }, i*3000);
+}
+
+function updateGameData() {
+  MLB_TEAMS_LOWERCASE.forEach(team => {
+    sleep(2000).then(() => {
+      csvToJson(team);
+    });
+  });
+}
+
+// updateGameData();
+
 // export default csvToJson;
 
