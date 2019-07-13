a63eba0d44287b8b5fa66a209ecaff6803550953
Caroline Wong <carowong5@gmail.com>
2019-07-13 16:37:07 -0700

    Updates

---
+++ precache-manifest.20fea0a904b47063ef538fd087bfb5fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6992650d46259b59520d7aad8a2ed91",
+    "url": "/game-today/index.html"
+  },
+  {
+    "revision": "e0440473eaa57126a28a",
+    "url": "/game-today/static/css/main.247e1461.chunk.css"
+  },
+  {
+    "revision": "789b2eaed1b0d7ca77a4",
+    "url": "/game-today/static/js/2.571795a5.chunk.js"
+  },
+  {
+    "revision": "e0440473eaa57126a28a",
+    "url": "/game-today/static/js/main.e59ab1a6.chunk.js"
+  },
+  {
+    "revision": "ae532604f7fa4e5ad2d2",
+    "url": "/game-today/static/js/runtime~main.a4e94e54.js"
+  }
+]);
